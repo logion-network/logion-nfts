f4514791a3e01f952df5bee151ef002f683c7d81
Benoit Devos <benoit@logion.network>
2023-10-10 12:10:48 +0200

    feat: add reusable logics to provide "tokenUri" contract message.
    logion-network/logion-internal#1013

---
+++ logics/impls/psp34_traits.rs
@@ -0,0 +1,33 @@
+use ink::prelude::{
+    string::{
+        String,
+        ToString,
+    },
+};
+use openbrush::{
+    contracts::psp34::extensions::{
+        metadata::{Internal, PSP34MetadataImpl},
+    },
+    contracts::psp34::Id,
+};
+
+use crate::traits::error::Error;
+
+pub trait Psp34Traits: PSP34MetadataImpl + Internal {
+
+    /// Change baseURI
+    // #[modifiers(only_owner)]
+    fn set_base_uri(&mut self, uri: String) -> Result<(), Error> {
+        self._set_attribute(Id::U8(0), String::from("baseURI"), uri);
+        Ok(())
+    }
+
+    /// Get URI from token ID
+    fn token_uri(&self, token_id: u64) -> String {
+        let value = self.get_attribute(Id::U8(0), String::from("baseURI"));
+        let mut token_uri = String::from_utf8(value.unwrap().into()).unwrap();
+        token_uri = token_uri + &token_id.to_string() + &String::from(".json");
+        token_uri
+    }
+
+}
---
+++ logics/traits/psp34_traits.rs
@@ -0,0 +1,15 @@
+use openbrush::traits::String;
+use crate::traits::error::Error;
+
+#[openbrush::wrapper]
+pub type Psp34TraitsContractRef = dyn Psp34TraitsContract;
+
+#[openbrush::trait_definition]
+pub trait Psp34TraitsContract {
+    /// This function sets the baseURI for the NFT contract. Only Contract Owner can perform this function. baseURI is the location of the metadata files if the NFT collection use external source to keep their NFT artwork. ArtZero uses IPFS by default, the baseURI can have format like this: ipfs://<hash_ID>/
+    #[ink(message)]
+    fn set_base_uri(&mut self, uri: String) -> Result<(), Error>;
+    /// This function return the metadata location of an NFT. The format is baseURI/<token_id>.json
+    #[ink(message)]
+    fn token_uri(&self, token_id: u64) -> String;
+}
